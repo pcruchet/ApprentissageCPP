6d29e369a6720efa88cb1fa12f6d26c54f3d68de
pcruchet <phcruchet@yahoo.fr>
2026-09-08 09:33:02 +0200

    feat:Réalisation d'un premier exercice sur les flux

---
+++ Medailles/main.cpp
@@ -0,0 +1,61 @@
+/************************************************************************************
+/*!
+    \file         main.cpp
+    \author    	  Philippe CRUCHET (Touchard Wahington le Mans)
+    \license      BSD (see license.txt)
+    \date         08 septembre 2017
+    \brief        Corrigé du TD1 - Les flux entrant en C++
+    \details      Mise en oeuvre des flux entrant et sortant sur fichier
+    \version  	  v1.0 - First release
+*/
+/*********************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <errno.h>
+
+using namespace std;
+
+int main()
+{
+    string inputFile;
+    string pays;
+    char tmp;
+    int nbOr;
+    int nbArgent;
+    int nbBronze;
+
+    cout << "Entrer le nom du fichier à lire : ";
+
+    cin >> inputFile;
+
+    ifstream fichier(inputFile.c_str()); //Creation du flux en lecture du fichier
+
+    if (fichier.fail())
+        cerr << "Erreur lors de l'ouverture du fichier" << endl;
+
+    else
+    {
+
+        //affichage de la premiere ligne du tableau
+    cout << setfill('-');
+        cout << "+" << setw(18) << "+" << setw(9) << "+" << setw(9) << "+" << setw(9) << "+" << endl;
+        cout << setfill(' ');
+
+        while(fichier)
+        {
+            fichier >> pays >> nbOr >> nbArgent >> nbBronze; //recuperation des valeurs
+            if(fichier)//Si le fichier peut etre lu
+            {
+                //affichage des lignes du tableau
+                cout << "| " << left << setw(16) << pays << "|" << right << setw(7) << nbOr << " |" << right << setw(7) << nbArgent << " |" << right << setw(7) << nbBronze << " |" << endl;
+            }
+        }
+        //Affichage du bas du tableau.
+        cout << setfill('-');
+        cout << "+" << setw(18) << "+" << setw(9) << "+" << setw(9) << "+" << setw(9) << "+" << endl;
+    }
+    return 0;
+}
